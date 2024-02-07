6451465778ddec983ff7ef43b51a68871dca4cd0
Christian Tricarico <c.tricarico@infotric.it>
2024-02-07 10:20:54 +0100

    Struttura clean architecture.

---
+++ Gdn.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Gdn.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
