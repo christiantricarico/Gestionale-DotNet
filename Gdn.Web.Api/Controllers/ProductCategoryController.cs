09d177a015a4d8ddefa64a58a66b307cb951e9ac
Christian Tricarico <c.tricarico@infotric.it>
2024-03-06 09:30:10 +0100

    Semplificati nomi action dei controllers.

--- Gdn.Web.Api/Controllers/ProductCategoryController.cs
+++ Gdn.Web.Api/Controllers/ProductCategoryController.cs
@@ -15,69 +15,69 @@ namespace Gdn.Web.Api.Controllers;
 [Route("api/[controller]")]
 public class ProductCategoryController : ControllerBase
 {
-    private readonly ISender _sender;
-    private readonly IMapper _mapper;
+	private readonly ISender _sender;
+	private readonly IMapper _mapper;
 
-    public ProductCategoryController(ISender sender, IMapper mapper)
-    {
-        _sender = sender;
-        _mapper = mapper;
-    }
+	public ProductCategoryController(ISender sender, IMapper mapper)
+	{
+		_sender = sender;
+		_mapper = mapper;
+	}
 
-    [HttpGet]
-    public async Task<IResult> GetAll()
-    {
-        var query = new GetProductCategoriesQuery();
-        var result = await _sender.Send(query);
+	[HttpGet]
+	public async Task<IResult> GetAll()
+	{
+		var query = new GetProductCategoriesQuery();
+		var result = await _sender.Send(query);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpGet("{id}")]
-    public async Task<IResult> GetProductCategory(int id)
-    {
-        var query = new GetProductCategoryByIdQuery(id);
-        var result = await _sender.Send(query);
+	[HttpGet("{id}")]
+	public async Task<IResult> Get(int id)
+	{
+		var query = new GetProductCategoryByIdQuery(id);
+		var result = await _sender.Send(query);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpPost("create")]
-    public async Task<IResult> CreateProductCategory(ProductCategoryInputModel model)
-    {
-        var input = _mapper.Map<ProductCategoryInput>(model);
-        var command = new CreateProductCategoryCommand(input);
-        var result = await _sender.Send(command);
+	[HttpPost("create")]
+	public async Task<IResult> Create(ProductCategoryInputModel model)
+	{
+		var input = _mapper.Map<ProductCategoryInput>(model);
+		var command = new CreateProductCategoryCommand(input);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.Created("", result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Created("", result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpPut("update")]
-    public async Task<IResult> UpdateProductCategory(ProductCategoryInputModel model)
-    {
-        var input = _mapper.Map<ProductCategoryInput>(model);
-        var command = new UpdateProductCategoryCommand(input);
-        var result = await _sender.Send(command);
+	[HttpPut("update")]
+	public async Task<IResult> Update(ProductCategoryInputModel model)
+	{
+		var input = _mapper.Map<ProductCategoryInput>(model);
+		var command = new UpdateProductCategoryCommand(input);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpDelete("{id}")]
-    public async Task<IResult> DeleteProductCategory(int id)
-    {
-        var command = new DeleteProductCatetoryCommand(id);
-        var result = await _sender.Send(command);
+	[HttpDelete("{id}")]
+	public async Task<IResult> Delete(int id)
+	{
+		var command = new DeleteProductCatetoryCommand(id);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.NoContent(),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.NoContent(),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 }
--- Gdn.Web.Api/Controllers/TaxRateController.cs
+++ Gdn.Web.Api/Controllers/TaxRateController.cs
@@ -15,69 +15,69 @@ namespace Gdn.Web.Api.Controllers;
 [Route("api/[controller]")]
 public class TaxRateController : ControllerBase
 {
-    private readonly ISender _sender;
-    private readonly IMapper _mapper;
+	private readonly ISender _sender;
+	private readonly IMapper _mapper;
 
-    public TaxRateController(ISender sender, IMapper mapper)
-    {
-        _sender = sender;
-        _mapper = mapper;
-    }
+	public TaxRateController(ISender sender, IMapper mapper)
+	{
+		_sender = sender;
+		_mapper = mapper;
+	}
 
-    [HttpGet]
-    public async Task<IResult> GetAll()
-    {
-        var query = new GetTaxRatesQuery();
-        var result = await _sender.Send(query);
+	[HttpGet]
+	public async Task<IResult> GetAll()
+	{
+		var query = new GetTaxRatesQuery();
+		var result = await _sender.Send(query);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpGet("{id}")]
-    public async Task<IResult> GetTaxRate(int id)
-    {
-        var query = new GetTaxRateByIdQuery(id);
-        var result = await _sender.Send(query);
+	[HttpGet("{id}")]
+	public async Task<IResult> Get(int id)
+	{
+		var query = new GetTaxRateByIdQuery(id);
+		var result = await _sender.Send(query);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpPost("create")]
-    public async Task<IResult> CreateTaxRate(TaxRateInputModel model)
-    {
-        var input = _mapper.Map<TaxRateInput>(model);
-        var command = new CreateTaxRateCommand(input);
-        var result = await _sender.Send(command);
+	[HttpPost("create")]
+	public async Task<IResult> Create(TaxRateInputModel model)
+	{
+		var input = _mapper.Map<TaxRateInput>(model);
+		var command = new CreateTaxRateCommand(input);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.Created("", result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Created("", result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpPut("update")]
-    public async Task<IResult> UpdateTaxRate(TaxRateInputModel model)
-    {
-        var input = _mapper.Map<TaxRateInput>(model);
-        var command = new UpdateTaxRateCommand(input);
-        var result = await _sender.Send(command);
+	[HttpPut("update")]
+	public async Task<IResult> Update(TaxRateInputModel model)
+	{
+		var input = _mapper.Map<TaxRateInput>(model);
+		var command = new UpdateTaxRateCommand(input);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.Ok(result.Data),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.Ok(result.Data),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 
-    [HttpDelete("{id}")]
-    public async Task<IResult> DeleteTaxRate(int id)
-    {
-        var command = new DeleteTaxRateCommand(id);
-        var result = await _sender.Send(command);
+	[HttpDelete("{id}")]
+	public async Task<IResult> Delete(int id)
+	{
+		var command = new DeleteTaxRateCommand(id);
+		var result = await _sender.Send(command);
 
-        return result.Match(
-            onSuccess: () => Results.NoContent(),
-            onFailure: () => Results.BadRequest(result.Error));
-    }
+		return result.Match(
+			onSuccess: () => Results.NoContent(),
+			onFailure: () => Results.BadRequest(result.Error));
+	}
 }
