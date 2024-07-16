97796c7fec29389d6eab6a237a1c243e5e5d41cb
Christian Tricarico <christiantric@hotmail.it>
2024-07-16 09:57:56 +0200

    Centralizzazione della creazione del HttpClient utilizzato nei razor components.

---
+++ Gdn.Web.Fluentblazor/Extensions/HttpClientFactoryExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Gdn.Web.Fluentblazor.Extensions;
+
+public static class HttpClientFactoryExtensions
+{
+    private const string CLIENT_NAME = "GdnWebApi";
+
+    public static HttpClient CreateApiClient(this IHttpClientFactory httpClientFactory)
+    {
+        return httpClientFactory.CreateClient(CLIENT_NAME);
+    }
+}
--- Gdn.Web.Fluentblazor/Program.cs
+++ Gdn.Web.Fluentblazor/Program.cs
@@ -7,18 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-//builder.Services.AddHttpClient("DefaultHttpClient", config =>
-//{
-//    config.BaseAddress = new Uri("https://localhost:7282/api");
-//});
-
-//builder.Services.AddScoped(sp =>
-//    new HttpClient
-//    {
-//        BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "https://localhost:7282/api")
-//    });
-
-builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("GdnWebApi", config =>
+{
+    config.BaseAddress = new Uri("https://localhost:7282/api/");
+});
 
 builder.Services.AddFluentUIComponents();
 
