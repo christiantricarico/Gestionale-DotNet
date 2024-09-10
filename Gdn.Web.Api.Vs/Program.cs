f5b3b29ef24d07779eceec76eb089834da789bad
Christian Tricarico <c.tricarico@infotric.it>
2024-09-10 09:33:30 +0200

    Aggiunto progetto web api con vertical slice architecture.

---
+++ Gdn.Web.Api.Vs/Program.cs
@@ -0,0 +1,69 @@
+using FluentValidation;
+using Gdn.Persistence;
+using Gdn.Web.Api.Vs.Endpoints;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddPersistence(options =>
+{
+    string? connectionString = builder.Configuration.GetConnectionString("SqlServerDefault");
+    options.UseSqlServer(connectionString);
+
+    if (builder.Environment.IsDevelopment())
+    {
+        options
+            .LogTo(message => Debug.WriteLine(message), LogLevel.Information)
+            .EnableSensitiveDataLogging();
+    }
+});
+
+builder.Services.AddEndpoints();
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapEndpoints();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
