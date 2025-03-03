39db977c174300761efee5f5dcc0f4178380dde4
Christian Tricarico <christiantric@hotmail.it>
2025-03-03 08:57:37 +0100

    Eliminazione aliquota iva e cliente dai rispettivi elenchi.

--- Gdn.Web.Api.Vs/Endpoints/ResultHelper.cs
+++ Gdn.Web.Api.Vs/Endpoints/ResultHelper.cs
@@ -4,6 +4,7 @@ namespace Gdn.Web.Api.Vs.Endpoints;
 
 public static class ResultHelper
 {
+    public static IResult NoContent() => TypedResults.NoContent();
     public static IResult Ok<T>(T data) where T : class => TypedResults.Ok(data);
     public static IResult Created<T>(T data) where T : class => TypedResults.Created(string.Empty, data);
     public static IResult BadRequest<T>(T data) where T : class => TypedResults.BadRequest(data);
---
+++ Gdn.Web.Api.Vs/Features/Customers/DeleteCustomer.cs
@@ -0,0 +1,26 @@
+﻿using Gdn.Domain.Data;
+using Gdn.Domain.Data.Repositories;
+using Gdn.Web.Api.Vs.Endpoints;
+
+namespace Gdn.Web.Api.Vs.Features.Customers;
+
+public class DeleteCustomer
+{
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("api/customers/{id:int}", Handler).WithTags(Tags.Customers);
+        }
+    }
+
+    private static async Task<IResult> Handler(int id, IUnitOfWork unitOfWork)
+    {
+        var customerRepository = unitOfWork.GetRepository<ICustomerRepository>();
+        await customerRepository.RemoveAsync(id);
+
+        await unitOfWork.SaveChangesAsync();
+
+        return ResultHelper.NoContent();
+    }
+}
