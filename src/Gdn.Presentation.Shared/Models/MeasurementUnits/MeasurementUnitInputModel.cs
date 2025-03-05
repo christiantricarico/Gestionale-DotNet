5b1936a0d83c2ede13121deb8ef45fced3e683bd
Christian Tricarico <christiantric@hotmail.it>
2025-03-05 09:54:27 +0100

    Frontend CRUD unità di misura.

---
+++ src/Gdn.Presentation.Shared/Models/MeasurementUnits/MeasurementUnitInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gdn.Presentation.Shared.Models.MeasurementUnits;
+
+public class MeasurementUnitInputModel
+{
+    public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Codice richiesto.")]
+    [StringLength(10)]
+    public string Code { get; set; } = default!;
+
+    [StringLength(255)]
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+}
