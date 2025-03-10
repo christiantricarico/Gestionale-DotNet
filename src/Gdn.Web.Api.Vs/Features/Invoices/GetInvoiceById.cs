a1af420eef82ca52ea6153923f44db0f21300a37
Christian Tricarico <christiantric@hotmail.it>
2025-03-10 08:40:38 +0100

    Implementazione features GetInvoices e GetInvoiceById in progetto Api.

---
+++ src/Gdn.Web.Api.Vs/Features/Invoices/GetInvoiceById.cs
@@ -0,0 +1,34 @@
+﻿using Gdn.Domain.Data.Repositories;
+using Gdn.Domain.Models;
+using Gdn.Web.Api.Vs.Endpoints;
+
+namespace Gdn.Web.Api.Vs.Features.Invoices;
+
+public class GetInvoiceById
+{
+    public record ResponseRow(long Id, string RowType, string? Description, decimal? Quantity, decimal? UnitPrice, int? MeasurementUnitId, int? TaxRateId);
+    public record Response(int Id, string Number, DateOnly Date, int CustomerId, IEnumerable<ResponseRow> Rows);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/invoices/{id}", Handler).WithTags(Tags.Invoices);
+        }
+    }
+
+    private static async Task<IResult> Handler(IInvoiceRepository invoiceRepository, int id)
+    {
+        var data = await invoiceRepository.GetAsync(id, ["Rows"]);
+
+        return data is not null
+            ? ResultHelper.Ok(MapResponse(data))
+            : ResultHelper.NotFound();
+    }
+
+    private static Response MapResponse(Invoice invoice)
+        => new(invoice.Id, invoice.Number, invoice.Date, invoice.CustomerId, invoice.Rows.Select(r => MapResponseRow(r)));
+
+    private static ResponseRow MapResponseRow(InvoiceRow row)
+        => new(row.Id, row.RowType, row.Description, row.Quantity, row.UnitPrice, row.MeasurementUnitId, row.TaxRateId);
+}
---
+++ src/Gdn.Web.Api.Vs/Features/Invoices/GetInvoices.cs
@@ -0,0 +1,33 @@
+﻿using Gdn.Domain.Data.Repositories;
+using Gdn.Domain.Models;
+using Gdn.Web.Api.Vs.Endpoints;
+
+namespace Gdn.Web.Api.Vs.Features.Invoices;
+
+public class GetInvoices
+{
+    public record ResponseRow(long Id, string RowType, string? Description, decimal? Quantity, decimal? UnitPrice, int? MeasurementUnitId, int? TaxRateId);
+    public record Response(int Id, string Number, DateOnly Date, int CustomerId, IEnumerable<ResponseRow> Rows);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/invoices", Handler).WithTags(Tags.Invoices);
+        }
+    }
+
+    private static async Task<IResult> Handler(IInvoiceRepository invoiceRepository)
+    {
+        var data = await invoiceRepository.GetAllAsync(["Rows"]);
+        var responseData = data.Select(e => MapResponse(e));
+
+        return ResultHelper.Ok(responseData);
+    }
+
+    private static Response MapResponse(Invoice invoice)
+        => new(invoice.Id, invoice.Number, invoice.Date, invoice.CustomerId, invoice.Rows.Select(r => MapResponseRow(r)));
+
+    private static ResponseRow MapResponseRow(InvoiceRow row)
+        => new(row.Id, row.RowType, row.Description, row.Quantity, row.UnitPrice, row.MeasurementUnitId, row.TaxRateId);
+}
