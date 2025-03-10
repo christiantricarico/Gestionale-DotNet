2f6d121b256ae9f3f4fe653f78b6e9ef5df49055
Christian Tricarico <christiantric@hotmail.it>
2025-03-10 08:32:41 +0100

    Implementazione feature DeleteInvoice in progetto api.

---
+++ src/Gdn.Web.Api.Vs/Features/Invoices/DeleteInvoice.cs
@@ -0,0 +1,26 @@
+﻿using Gdn.Domain.Data;
+using Gdn.Domain.Data.Repositories;
+using Gdn.Web.Api.Vs.Endpoints;
+
+namespace Gdn.Web.Api.Vs.Features.Invoices;
+
+public class DeleteInvoice
+{
+    public sealed class Endpoint : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("api/invoices/{id:int}", Handler).WithTags(Tags.Invoices);
+        }
+    }
+
+    private static async Task<IResult> Handler(int id, IUnitOfWork unitOfWork)
+    {
+        var invoiceRepository = unitOfWork.GetRepository<IInvoiceRepository>();
+        await invoiceRepository.RemoveAsync(id);
+
+        await unitOfWork.SaveChangesAsync();
+
+        return ResultHelper.NoContent();
+    }
+}
