52b867b0e5c2de785a95b2296f861b1a93c2ae1f
Christian Tricarico <c.tricarico@infotric.it>
2025-04-05 11:31:08 +0200

    Dati indirizzo in creazione e modifica cliente su progetto frontend.

---
+++ src/Gdn.Web.Fluentblazor/Models/Customers/CustomerEditModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gdn.Web.Fluentblazor.Models.Customers;
+
+public class CustomerEditModel
+{
+    public int? Id { get; set; }
+
+    [Required(ErrorMessage = "Codice richiesto.")]
+    [StringLength(10)]
+    public string Code { get; set; } = default!;
+
+    [StringLength(255)]
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    [StringLength(20)]
+    public string? FiscalCode { get; set; }
+
+    [StringLength(20)]
+    public string? VatNumber { get; set; }
+
+    [StringLength(50)]
+    public string? Phone { get; set; }
+
+    [StringLength(255)]
+    public string? Email { get; set; }
+
+    [StringLength(255)]
+    public string? Website { get; set; }
+
+    [StringLength(255)]
+    public string? Pec { get; set; }
+
+    [StringLength(10)]
+    public string? Sdi { get; set; }
+
+    public string? Notes { get; set; }
+
+    [StringLength(255)]
+    public string? Street { get; set; }
+
+    [StringLength(10)]
+    public string? PostalCode { get; set; }
+
+    [StringLength(50)]
+    public string? City { get; set; }
+
+    [StringLength(50)]
+    public string? Province { get; set; }
+
+    [StringLength(50)]
+    public string? Country { get; set; }
+}
