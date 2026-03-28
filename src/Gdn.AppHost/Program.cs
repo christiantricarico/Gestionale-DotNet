2797b22ad76276296519943cc5b8a3064d8381a5
Christian Tricarico <c.tricarico@infotric.it>
2026-03-28 15:42:32 +0100

    Aggiunto progetto Gdn.Web.MudBlazor.

--- src/Gdn.AppHost/Program.cs
+++ src/Gdn.AppHost/Program.cs
@@ -1,7 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Gdn_Web_Fluentblazor>("gdn-web-fluentblazor");
-
+builder.AddProject<Projects.Gdn_Web_MudBlazor>("gdn-web-mudblazor");
 builder.AddProject<Projects.Gdn_Web_Api_Vs>("gdn-web-api-vs");
 
 builder.Build().Run();
